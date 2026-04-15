2eb0ca2bbf8a5a02760f176a3b5f2a7a5ebc75e0
Karol Foltyn <s33765@pjwstk.edu.pl>
2026-04-15 11:20:58 +0200

    Brute Force to check

--- src/Main.java
+++ src/Main.java
@@ -41,12 +41,41 @@ public class Main {
         listToTeach = new ArrayList<>();
         listToTest = new ArrayList<>();
 
-        TextFile tf = new TextFile("src/TextFiles/PL1", "Polski");
-
-        double[] input = tf.getInput();
-        for(int i = 0; i < input.length; i++){
-            System.out.println(input[i]);
-        }
+        listToTeach.add(new TextFile("src/TextFiles/PL1.txt", "Polski"));
+        listToTeach.add(new TextFile("src/TextFiles/PL2.txt", "Polski"));
+        listToTeach.add(new TextFile("src/TextFiles/PL3.txt", "Polski"));
+        listToTeach.add(new TextFile("src/TextFiles/PL4.txt", "Polski"));
+        listToTest.add(new TextFile("src/TextFiles/PL5.txt", "Polski"));
+        listToTest.add(new TextFile("src/TextFiles/PL6.txt", "Polski"));
+        listToTest.add(new TextFile("src/TextFiles/PL7.txt", "Polski"));
+        listToTeach.add(new TextFile("src/TextFiles/EN1.txt", "Angielski"));
+        listToTeach.add(new TextFile("src/TextFiles/EN2.txt", "Angielski"));
+        listToTeach.add(new TextFile("src/TextFiles/EN3.txt", "Angielski"));
+        listToTeach.add(new TextFile("src/TextFiles/EN4.txt", "Angielski"));
+        listToTest.add(new TextFile("src/TextFiles/EN5.txt", "Angielski"));
+        listToTest.add(new TextFile("src/TextFiles/EN6.txt", "Angielski"));
+        listToTest.add(new TextFile("src/TextFiles/EN7.txt", "Angielski"));
+        listToTeach.add(new TextFile("src/TextFiles/DE1.txt", "Niemiecki"));
+        listToTeach.add(new TextFile("src/TextFiles/DE2.txt", "Niemiecki"));
+        listToTeach.add(new TextFile("src/TextFiles/DE3.txt", "Niemiecki"));
+        listToTeach.add(new TextFile("src/TextFiles/DE4.txt", "Niemiecki"));
+        listToTest.add(new TextFile("src/TextFiles/DE5.txt", "Niemiecki"));
+        listToTest.add(new TextFile("src/TextFiles/DE6.txt", "Niemiecki"));
+        listToTest.add(new TextFile("src/TextFiles/DE7.txt", "Niemiecki"));
+        listToTeach.add(new TextFile("src/TextFiles/FR1.txt", "Francuski"));
+        listToTeach.add(new TextFile("src/TextFiles/FR2.txt", "Francuski"));
+        listToTeach.add(new TextFile("src/TextFiles/FR3.txt", "Francuski"));
+        listToTeach.add(new TextFile("src/TextFiles/FR4.txt", "Francuski"));
+        listToTest.add(new TextFile("src/TextFiles/FR5.txt", "Francuski"));
+        listToTest.add(new TextFile("src/TextFiles/FR6.txt", "Francuski"));
+        listToTest.add(new TextFile("src/TextFiles/FR7.txt", "Francuski"));
+        listToTeach.add(new TextFile("src/TextFiles/ES1.txt", "Hiszpański"));
+        listToTeach.add(new TextFile("src/TextFiles/ES2.txt", "Hiszpański"));
+        listToTeach.add(new TextFile("src/TextFiles/ES3.txt", "Hiszpański"));
+        listToTeach.add(new TextFile("src/TextFiles/ES4.txt", "Hiszpański"));
+        listToTest.add(new TextFile("src/TextFiles/ES5.txt", "Hiszpański"));
+        listToTest.add(new TextFile("src/TextFiles/ES6.txt", "Hiszpański"));
+        listToTest.add(new TextFile("src/TextFiles/ES7.txt", "Hiszpański"));
 
     }
 }
